0a2d5ff8868dd8f86b22bcc8c68d17dc18b5a901
Jin <jbjxu@thoughtworks.com>
2019-09-22 12:31:26 +0800

    refactor: style update(make tabbar content to be center of the heafer)

--- apps/packages/app-superfluid/src/componments/Selection/index.tsx
+++ apps/packages/app-superfluid/src/componments/Selection/index.tsx
@@ -14,6 +14,7 @@ import {withApi} from '@polkadot/react-api';
 import Swap from './Swap';
 import Pool from './Pool';
 import translate from '../../translate';
+import './style.css'
 
 interface Props extends ApiProps, I18nProps {
   basePath: string;
